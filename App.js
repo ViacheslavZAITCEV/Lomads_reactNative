005b2197a7f0a12b6b168242cafe392a79cc67d5
Mike Ostrowski <mickael.ostrowski@gmail.com>
2020-12-07 10:04:23 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+console.disableYellowBox = true;
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import AfficheMainScreen from './screens/AfficheMainScreen';
+import FriendsMainScreen from './screens/FriendsMainScreen';
+import MessagesMainScreen from './screens/MessagesMainScreen';
+import PlanMainScreen from './screens/PlanMainScreen';
+import ProfileMainScreen from './screens/ProfileMainScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import {createAppContainer } from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import { Entypo, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+
+var BottomNavigator = createBottomTabNavigator(
+  {
+    "À l'affiche": AfficheMainScreen,
+    Messages: MessagesMainScreen,
+    Planifier: PlanMainScreen,
+    Amis: FriendsMainScreen,
+    Profil: ProfileMainScreen
   },
-});
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        var iconCode;
+        if (navigation.state.routeName == "À l'affiche") {
+          iconCode = <MaterialCommunityIcons name="bulletin-board" size={25} color={tintColor} />
+        } else if (navigation.state.routeName == 'Messages') {
+          iconCode = <Entypo name='chat' size={25} color={tintColor} />
+        } else if (navigation.state.routeName == 'Planifier') {
+          iconCode = <MaterialCommunityIcons name='calendar-heart' size={25} color={tintColor} />
+        } else if (navigation.state.routeName == 'Amis') {
+          iconCode = <MaterialCommunityIcons name="account-supervisor-circle" size={25} color={tintColor} />
+        } else if (navigation.state.routeName == 'Profil') {
+          iconCode = <FontAwesome name='user-circle' size={21} color={tintColor} />
+        }
+        return iconCode;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#EFB509',
+      inactiveTintColor: '#F8F5F2',
+      activeBackgroundColor: '#16253D',
+      inactiveBackgroundColor: '#16253D',
+    },
+  }
+);
+
+var Navigation = createAppContainer(BottomNavigator);
+export default Navigation;

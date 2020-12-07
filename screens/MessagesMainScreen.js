941ddf644554cecaaf7676335172225a7b3ea42c
Mike Ostrowski <mickael.ostrowski@gmail.com>
2020-12-07 09:24:32 +0100

    main screens

---
+++ screens/MessagesMainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+export default function MessagesMainScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Messages main screen
+      </Text>
+    </View>
+  );
+}
---
+++ screens/PlanMainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+export default function PlanMainScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Plan main screen
+      </Text>
+    </View>
+  );
+}

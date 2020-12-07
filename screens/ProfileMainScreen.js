638856e9542327b4f63fae20f2e5239431d04907
Mike Ostrowski <mickael.ostrowski@gmail.com>
2020-12-07 18:36:58 +0100

    profile main screen work in progress

--- screens/ProfileMainScreen.js
+++ screens/ProfileMainScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
-import { Header, Text, Avatar, Accessory } from 'react-native-elements';
+import { Header, Text, Avatar, Divider } from 'react-native-elements';
 
 import { AntDesign, FontAwesome } from '@expo/vector-icons';
 
@@ -17,17 +17,21 @@ export default function ProfileMainScreen() {
       />
 
       <Avatar
-        size="xlarge"
+        size='xlarge'
+        marginTop={15}
+        marginBottom={10}
         rounded
-        containerStyle={{marginTop: 25}}
-        icon={{name: 'user', type: 'font-awesome'}}
         source={{
           uri:
-            require('../assets/avatar-1.jpg'),
+            'https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2019/05/justice-league-retour-a-un-batman-plus-traditionnel-et-heroique-une-e1559338081214.jpg',
         }}
       />
-      <Text h4>John Doe</Text> 
+      <Text h4>Bruce Wayne</Text> 
       <Text h5>Paris, FR</Text> 
+
+      <Divider marginTop={10} marginBottom={10} style={{ backgroundColor: '#16253D', width: 250, height: 2 }} />
+
+      <Text h6 fontWeight='bold'>Mes préférences</Text> 
 
       <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>

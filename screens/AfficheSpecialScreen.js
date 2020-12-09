0a63112736908947b1d0b4b71ae40a7cfe509c35
emmanuelle bouaziz <bouazizemmanuelle99@gmail.com>
2020-12-09 14:20:04 +0100

    redux affiche special screen

--- screens/AfficheSpecialScreen.js
+++ screens/AfficheSpecialScreen.js
@@ -1,29 +1,53 @@
-import React from 'react';
-import { View, ScrollView, Button } from 'react-native';
-import { Text } from 'react-native-elements';
+import React, {useState, useEffect} from "react";
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  Text,
+  Input,
+  Button,
+  Image
+  
+} from 'react-native-elements';
 
 import HeaderWithAvatar from './HeaderWithAvatar';
 
-export default function AfficheSpecialScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center' }}>
+import {connect} from 'react-redux';
+
+
+
+function AfficheSpecialScreen(props) {
+  const [evenement,setEvenement] = useState('') 
+  useEffect(() => {
+    const findEvent = async() => {
+      const data = await fetch(`https//192.168.1.142:3000/pullEventDetaille`, {
+        method: 'POST',
+        headers: {'Content-Type':'application/x-www-form-urlencoded'},
+        body: 'id='+props.idEvent
+      });
+
+      const body = await data.json()
+      console.log(body)
+      setEvenement (body) 
+    }
+    findEvent ()    
+  },[])
+
+  var event = evenement.map((event,i) => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center' }}>
 
       <HeaderWithAvatar />
 
-      <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
         <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
-          <Image style={{ width: 150, height: 210, margin: 5 }} source={{ uri: 'https://weheartit.com/entry/350627019?context_query=affiche+film&context_type=search' }} />
-          <Text style={{ textAlign: 'center', marginTop: 3 }}>BLACK SWAN</Text>
-          <Fab disabled aria-label="like"
-            size="small"
-            color="#D70026">
-            <FavoriteIcon />
-          </Fab>
+          <Image style={{ width: 150, height: 210, margin: 5 }} source={{ uri: event.image }} />
+          <Text style={{ textAlign: 'center', marginTop: 3 }}>{event.nom}</Text>
+         
 
         </View>
         <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
-          <Text style={{ textAlign: 'center', marginTop: 3 }}>Thomas Leroy, le directeur du New York City Ballet, est à la recherche d'une nouvelle danseuse étoile depuis qu'il a décidé de se passer des services de la titulaire, Beth. Nina, depuis longtemps membre de la troupe, pense que son heure est venue. Une rivale inattendue se présente en la personne de la sensuelle Lily, nouvellement recrutée. Nina peut compter sur sa mère, Erica, une maîtresse-femme qui contrôle sa vie, pour intervenir auprès de Thomas.</Text>
+          <Text style={{ textAlign: 'center', marginTop: 3 }}>{event.description}</Text>
 
         </View>
 
@@ -34,21 +58,22 @@ export default function AfficheSpecialScreen({ navigation }) {
 
         <View style={{ alignItems: 'center', width: 150, margin: 5 }}></View>
 
-      </ScrollView>
+     
 
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction input="PLANIFIER" />
-        <BottomNavigationAction input="MESSAGES" />
-        <BottomNavigationAction input="Réserver" />
-
-      </BottomNavigation>
-
+  
     </View>
+    )
+  })
+  return (
+    
   );
 }
+function mapStateToProps(state){
+  return {idEvent: state.idEvent}
+}
+
+
+export default connect(
+  mapStateToProps,
+null,
+)(AfficheSpecialScreen);

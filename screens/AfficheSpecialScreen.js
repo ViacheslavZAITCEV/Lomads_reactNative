ba52a44661a9fe0fe893ab3fd71e42688b90b9c7
Alexandra BC <alexandra.catel@gmail.com>
2020-12-14 14:58:39 +0100

    commit avant un npx bizarre

--- screens/AfficheSpecialScreen.js
+++ screens/AfficheSpecialScreen.js
@@ -11,7 +11,7 @@ import {
   Button,
   Image,  
 
-  
+
 } from 'react-native-elements';
 import { AntDesign } from '@expo/vector-icons';
 const styles = StyleSheet.create({
@@ -24,14 +24,23 @@ const styles = StyleSheet.create({
 
 import {Picker} from '@react-native-picker/picker';
 
+import Heart from './components/cardEvenement'
+
 import {connect} from 'react-redux';
 
+import urlLocal from '../urlDevsGoWizMe'
+
 function AfficheSpecialScreen(props) {
-  const [evenement,setEvenement] = useState({})    
+  const [evenement,setEvenement] = useState({})
   const [lieuEvenementSansDoublons,setLieuEvenementSansDoublons] = useState([]);
   const [selectLieuEvenement,setSelectLieuEvenement] = useState('');
   const [dateEvenement,setDateEvenement] = useState([]);
   const [selectDateEvenement,setSelectDateEvenement] = useState('');
+  const [user, setUser] = useState(null);
+
+
+  const imageBackground = { uri: "https://us.123rf.com/450wm/zephyr18/zephyr181509/zephyr18150900028/44975226-nature-abstraite-arri%C3%A8re-plan-flou-dans-un-ton-bleu-turquoise-avec-un-soleil-%C3%A9clatant-des-reflets-et-un-.jpg?ver=6" };
+
   var lieuTransit=[];
   var lieux;
   var uniqueset;
@@ -40,79 +49,109 @@ function AfficheSpecialScreen(props) {
   var dates;
 
   useEffect(() => {
+    const updateUser = async () => {
+        if(props.token){
+
+    const userBD = await fetch(`${urlLocal}/users/getUser`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `token=${props.token}`
+    })
+    const body = await userBD.json();
+    console.log('AfficheSpecialScreen, updateUser(), user = ', body);
+    setUser(body);
+    // setCurrentCity(body.ville);
+    }
+}
+updateUser ();
+  },[props.token])
+
+  useEffect(() => {
     const findEvent = async() => {
       console.log("PROPS RECU", props.idEvent)
-      const data = await fetch(`http://172.17.1.91:3000/pullEventDetaille`, {
+      const data = await fetch(`${urlLocal}/pullEventDetaille`, {
         method: 'POST',
         headers: {'Content-Type':'application/x-www-form-urlencoded'},
         body: 'id='+props.idEvent});
 
-      const body = await data.json()
-      
-      setEvenement(body) 
-      console.log(">>>>>>>>>>>>>>>><<<<<<<", evenement.lieux_dates);
-      console.log("=========>", evenement.lieux_dates[0].duree);
-    };
-    async function representations() {
-      recupLieu();
-        // console.log("LIEU", lieuTransit)
-        // console.log("LIEU nombre", lieuTransit.length)
-        // console.log("backarray ", backarray)
-        // console.log("lieux", lieux)
-       };
-    findEvent() ;
-    representations();   
+  const body = await data.json()
+  setEvenement(body);
+  console.log("body", body);
+  
+  console.log("event", body);
+  backarray = recupLieu(body);
+  setLieuEvenementSansDoublons(backarray);
+  createLieuPicker(backarray);
+  
+  console.log("backarray ", backarray);
+  console.log("LIEU", lieuTransit)
+  console.log("LIEU nombre", lieuTransit.length)
+  console.log("lieux", lieux)      
+};
+findEvent() ;
   },[])
 
-   useEffect(() => {
-        async function horairesEvent() {
-        horaires()
-      };
-     horairesEvent();
+  useEffect(() => {
+    async function horairesEvent() {
+      setSelectDateEvenement('');
+      horaires()
+    };
+    horairesEvent();
   },[selectLieuEvenement])
 
-
-
-
-  var recupLieu = () => {
-    for (var i = 0; i< evenement.lieux_dates.length;i++) {
-      lieuTransit.push(evenement.lieux_dates[i].salle)
+  function recupLieu (event){
+    var backy = [];
+    if (event.lieux_dates){
+      console.log ('evenement.lieux_dates = ', event.lieux_dates)
+      for (var i = 0; i < event.lieux_dates.length;i++) {
+        lieuTransit.push(event.lieux_dates[i].salle)
+      }
+      uniqueset = new Set(lieuTransit)
+      backy=[...uniqueset];
+      console.log('backarray = ', backy);
     }
-    uniqueset = new Set(lieuTransit)
-    backarray=[...uniqueset];
-    setLieuEvenementSansDoublons(backarray)
-    console.log("=========>>>>>",lieuEvenementSansDoublons[i])
+    return backy;
   }
+
  
    var horaires= ()=>{
-    // console.log("coucou")
+    console.log("coucou from function 'horares'");
     var dateFormat = function(date){
       var newDate = new Date(date);
       var format = newDate.getDate()+'/'+(newDate.getMonth()+1)+'/'+newDate.getFullYear()+' - '+newDate.getHours()+'h'+newDate.getMinutes();
-      console.log("FORMAT<<<<<<<<<",format)
       return format;
     }
-    for (var i = 0; i< evenement.lieux_dates.length;i++) {
-      if(selectLieuEvenement.itemValue==evenement.lieux_dates[i].salle) {
-        horaireTransit.push(dateFormat(evenement.lieux_dates[i].date_debut))
+    if (evenement.lieux_dates){
+      for (var i = 0; i< evenement.lieux_dates.length; i++) {
+        if(selectLieuEvenement.itemValue==evenement.lieux_dates[i].salle) {
+          horaireTransit.push(dateFormat(evenement.lieux_dates[i].date_debut));
       }}
-      // console.log("Nous mets un undefined lorqu'on console.log(horaireTransit)")
-    setDateEvenement(horaireTransit)}
+    }
+    console.log("HORAIRE TRANSIT",horaireTransit);
+    setDateEvenement(horaireTransit);
+  }
 
-  lieux = lieuEvenementSansDoublons.map((lieu,i)=>{
-    return(<Picker.Item key={i} label={lieu} value={lieu} />)})
- 
-  // console.log("selectLieuEvenement",selectLieuEvenement)
+  if (lieuEvenementSansDoublons.length > 0){
+    createLieuPicker(lieuEvenementSansDoublons);
+  }
 
-  dates = dateEvenement.map((date,i)=>{
-    return(<Picker.Item key={i} label={date} value={date} />)
-  });
-  var duree = evenement.lieux_dates[0].duree;
+  function createLieuPicker (lieuEvenementSansDoublons){
+    lieux = lieuEvenementSansDoublons.map((lieu,i)=>{
+      return(
+      <Picker.Item 
+        key={i} 
+        label={lieu} 
+        value={lieu} 
+      />)
+    })
+  }
+  console.log("selectLieuEvenement",selectLieuEvenement)
 
-  // console.log("DATES++++++++ ", dateEvenement)
-  
-  const imageBackground = { uri: "https://us.123rf.com/450wm/zephyr18/zephyr181509/zephyr18150900028/44975226-nature-abstraite-arri%C3%A8re-plan-flou-dans-un-ton-bleu-turquoise-avec-un-soleil-%C3%A9clatant-des-reflets-et-un-.jpg?ver=6" };
-
+  if (dateEvenement.length > 0){
+    dates = dateEvenement.map((date,i)=>{
+      return(<Picker.Item key={i} label={date} value={date} />)
+    });
+  }
 
     return (
       <ScrollView style={{ flex: 1}}>
@@ -122,105 +161,123 @@ function AfficheSpecialScreen(props) {
       
         <View style={{ flex:1, alignItems: 'center', width: 300, margin: 5 }}>
           <Image style={{ width: 150, height: 210, margin: 5 }} source={{ uri: evenement.image }} />
-          <AntDesign
-            name="heart"
-            size={25}
-            color='#D70026'
-            style={{ position: 'absolute', top: 5, left: 270 }}
-            onPress={() => console.log("LIKÉ")}
-          />
-          <Text style={{ textAlign: 'center', marginTop: 3,fontWeight:'bold' }}>{evenement.nom}</Text>
-         
 
-        </View>
-        <View style={{ alignItems: 'center', width: 300, margin: 5 }}>
-          <Text style={{ textAlign: 'center', marginTop: 3 }}>{evenement.description}</Text>
+      <Heart
+        size={25}
+        style={{ position: 'absolute', top: 5, left: 140 }}
+        token={props.token}      
+        i={0}
+        x={evenement}
+        user={user}
+        navigation={props.navigation}
+      />
 
-        </View>
 
-        <View style={{ alignItems: 'center', width: 500, margin: 5 }}>
-          <Text style={{ textAlign: 'center', marginTop: 3 }}>Type : {evenement.type}</Text>
 
-        </View>
+      {/* <AntDesign
+        name="heart"
+        size={25}
+        color='#D70026'
+        onPress={() => console.log("LIKÉ")}
+      /> */}
+      <Text style={{ textAlign: 'center', marginTop: 3,fontWeight:'bold' }}>{evenement.nom}</Text>
+     
 
-        <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
-          <Text style={{ textAlign: 'center', marginTop: 3 }}>Catégories : {evenement.categories} </Text>
-        </View>
-
-        <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
-          <Text style={{ textAlign: 'center', marginTop: 3 }}>Durée : {duree} minutes</Text>
-          
-        </View>
-
-        <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
-      
-        <Picker
-          selectedValue={lieuTransit}
-         style={{height: 50, width: 200}}
-         onValueChange={(itemValue, itemIndex) =>
-          setSelectLieuEvenement({itemValue})
-          }>
-          <Picker.Item  label='Lieu(x)' value='Lieux' />
-         {lieux}
-        </Picker>
-        </View>
-        <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
-       
-        <Picker
-        onPress={() => {horaires()}}
-        selectedValue={horaireTransit}
-         style={{height: 50, width: 200}}
-         onValueChange={(itemValue, itemIndex) =>
-          setSelectDateEvenement({itemValue})
-          }>
-           <Picker.Item label='Date(s) et horaire(s)' value='2020-12-24T00:00:00.000Z' />
-         {dates}
-        </Picker>
-        </View>
-        
-        <View style={{ alignItems: 'center', width: 200, margin: 5 }}>
-        <Text style={{ textAlign: 'center', marginTop: 3 }}>Votre choix de lieu : </Text>
-        <Text style={{ textAlign: 'center', marginTop: 3,fontWeight:'bold',color: "#16253D"  }}>{selectLieuEvenement.itemValue}</Text>
-        <Text style={{ textAlign: 'center', marginTop: 8 }}>Votre choix de séance : </Text>
-        <Text style={{ textAlign: 'center', marginTop: 3,fontWeight:'bold', color: "#16253D" }}>{selectDateEvenement.itemValue}</Text>
-        </View>
-
-        <View style={{ flex:1, alignItems: 'center', width: 200, margin: 5 }}>
-        <Button
-              type='outline'
-              title="Créer une sortie"
-              buttonStyle={{ backgroundColor: "#D70026", marginBottom:0,width: 150, margin: 5 }}
-              titleStyle={{ color: 'white' }}
-              onPress={() => {
-                console.log(">>>>>>>>>>>>>>>>>>>>>>SORTIE");
-                
-              }}            
-            />
-            <Button
-              type='outline'
-              title="Réserver"
-              buttonStyle={{ backgroundColor: "#D70026", marginBottom:0,  width: 150, margin: 5 }}
-              titleStyle={{ color: 'white' }}
-              onPress={() => {
-                console.log(">>>>>>>>>>>>>>>>>>>>>>RESERVER");
-                
-              }}            
-            />
-
-        </View>
-    
-    </ImageBackground>
-   
     </View>
-    </ScrollView>
-    )
-  
+    <View style={{ alignItems: 'center', width: 300, margin: 5 }}>
+      <Text style={{ textAlign: 'center', marginTop: 3 }}>{evenement.description}</Text>
 
+    </View>
+
+    <View style={{ alignItems: 'center', width: 500, margin: 5 }}>
+      <Text style={{ textAlign: 'center', marginTop: 3 }}>Type : {evenement.type}</Text>
+
+    </View>
+
+    <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
+      <Text style={{ textAlign: 'center', marginTop: 3 }}>
+        Catégories : 
+        {evenement.categories} 
+      </Text>
+
+    </View>
+
+    <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
+      <Text style={{ textAlign: 'center', marginTop: 3 }}>Durée : { (evenement && evenement.lieux_dates && evenement.lieux_dates.length >0) ? evenement.lieux_dates[0].duree : ''}  minutes</Text>
+
+    </View>
+
+    <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
+  
+    <Picker
+      selectedValue={lieuTransit}
+     style={{height: 50, width: 200}}
+     onValueChange={(itemValue, itemIndex) =>
+      setSelectLieuEvenement({itemValue})
+      }>
+      <Picker.Item  label='Lieu(x)' value='Lieux' />
+     {lieux}
+    </Picker>
+    </View>
+    <View style={{ alignItems: 'center', width: 150, margin: 5 }}>
+   
+    <Picker
+    onPress={() => {horaires()}}
+    selectedValue={horaireTransit}
+     style={{height: 50, width: 200}}
+     onValueChange={(itemValue, itemIndex) =>
+      setSelectDateEvenement({itemValue})
+      }>
+       <Picker.Item label='Date(s) et horaire(s)' value='2020-12-24T00:00:00.000Z' />
+     {dates}
+    </Picker>
+    </View>
+    
+    <View style={{ alignItems: 'center', width: 200, margin: 5 }}>
+    <Text style={{ textAlign: 'center', marginTop: 3 }}>Votre choix de lieu : </Text>
+    <Text style={{ textAlign: 'center', marginTop: 3,fontWeight:'bold',color: "#16253D"  }}>{selectLieuEvenement.itemValue}</Text>
+    <Text style={{ textAlign: 'center', marginTop: 8 }}>Votre choix de séance : </Text>
+    <Text style={{ textAlign: 'center', marginTop: 3,fontWeight:'bold', color: "#16253D" }}>{selectDateEvenement.itemValue}</Text>
+    </View>
+
+    <View style={{ flex:1, alignItems: 'center', width: 200, margin: 5 }}>
+    <Button
+          type='outline'
+          title="Créer une sortie"
+          buttonStyle={{ 
+            backgroundColor: (selectLieuEvenement !== '' && selectDateEvenement !== '') ? "#D70026" : "#16253D", 
+            marginBottom:0,width: 150, margin: 5 }}
+          titleStyle={{ color: 'white' }}
+          onPress={() => {
+            console.log(">>>>>>>>>>>>>>>>>>>>>>SORTIE");
+            
+          }}            
+        />
+        <Button
+          type='outline'
+          title="Réserver"
+          buttonStyle={{ 
+            backgroundColor: (selectLieuEvenement !== '' && selectDateEvenement !== '') ? "#D70026" : "#16253D", 
+            marginBottom:0,  width: 150, margin: 5 }}
+          titleStyle={{ color: 'white' }}
+          onPress={() => {
+            console.log(">>>>>>>>>>>>>>>>>>>>>>Reserver");
+            
+          }}            
+        />
+
+    </View>
+
+</ImageBackground>
+   
+</View>
+</ScrollView>
+)
+  
 }
 function mapStateToProps(state){
   return {idEvent: state.idEventReducer}
 }
-
 
 export default connect(
   mapStateToProps,

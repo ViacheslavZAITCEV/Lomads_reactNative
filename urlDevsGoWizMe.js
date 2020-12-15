3ea2512c786afa18fa366803d759f335218cacc9
Mike Ostrowski <mickael.ostrowski@gmail.com>
2020-12-15 08:58:05 +0100

    changement ip Mike

--- urlDevsGoWizMe.js
+++ urlDevsGoWizMe.js
@@ -1,5 +1,5 @@
-const urlLocal='http://192.168.0.14:3000';
+const urlLocal='http://172.20.10.9:3000';
 // ipMikeHome: 192.168.0.14
-// const urlLocal='http://192.168.1.98:3000';
+// ipMikeCellphone: 172.20.10.9
 
 export default urlLocal;

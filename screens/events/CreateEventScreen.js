6128394bb6e3f258321d6e2b2d88b74841b6d497
Viacheslav ZAITCEV <zaitcev@mail.fr>
2021-06-13 11:00:33 +0200

    npm audit fix

--- screens/events/CreateEventScreen.js
+++ screens/events/CreateEventScreen.js
@@ -8,28 +8,45 @@ import { FontAwesome } from '@expo/vector-icons';
 import ModalComponent from '../components/Modal'
 import InputComponent from '../components/InputComponent';
 import ChooseType from './components/ChooseType';
+import ChooseDate from './components/ChooseDate';
 
 
 function newEventScreen (){
 
   const [eventName, setEventName] = useState('')
   const [description, setDescription] = useState('')
+  const [address, setAddress] = useState('')
   const [type, setType] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [dateFin, setDateFin] = useState(new Date())
 
   const [toModal, setToModal]= useState({})
   const [modal, setModal] = useState(false)
   
+  useEffect ( ()=>{
+
+  })
+  const getDate = ()=>{
+    console.log('typeof date',typeof date)
+    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' - ' + date.getHours() + 'h0' + date.getMinutes();
+  }
+
   const modalOn = (type, message)=>{
       setToModal( {type, message, setModal, modal} )   
       setModal(true)
   }
 
   
+  var changeDate = async () => {
+    console.warn('changeDate Event Button is pressed')
+
+  }
+  
   var handleSubmit = async () => {
     console.warn('New Event Button is pressed')
   }
   return (
-    <View  >
+    <View style={{ width: "95%", justifyContent: 'space-around' }} >
       <ModalComponent 
       type={toModal.type}
       message={toModal.message}
@@ -46,10 +63,34 @@ function newEventScreen (){
                 <InputComponent 
                 placeholder="description"
                 setState = { setDescription} />
+                <InputComponent 
+                placeholder="address"
+                setState = { setAddress} />
             </SafeAreaView>
         </KeyboardAvoidingView>
       </ScrollView>
       <ChooseType  setState={setType} type={type} />
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', width: '95%'}}>
+        <View>
+        <Text style={{color: '#353C3F',justifyContent: 'center', fontSize: 18, fontWeight: 'bold'}} >
+          Date : 
+        </Text>
+        <Text style={{color: '#353C3F',justifyContent: 'center', fontSize: 18, fontWeight: 'bold'}} >
+          {getDate()}
+        </Text>
+        </View>
+        <TouchableOpacity
+            onPress={() => changeDate()}
+            style={{
+                width: '40%', height: 40, backgroundColor: '#FF8200',
+                 alignItems: 'center', justifyContent: 'center', borderRadius: 15
+            }}
+        >
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>Change Date</Text>
+        </TouchableOpacity>
+      </View>
+
       <View style={{ alignItems: 'center' }}>
         <TouchableOpacity
             onPress={() => handleSubmit()}
